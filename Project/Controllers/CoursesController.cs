be6e392d175624e7badadacbd7c4e33b6897add6
Metehan İNAL <metehan.inal25@gmail.com>
2023-10-03 10:05:58 +0300

    create files is created

---
+++ Project/Controllers/CoursesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project.Business.Abstract;
+
+namespace Project.Controllers
+{
+    public class CoursesController : Controller
+    {
+        ICourseService _courseService;
+        public CoursesController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+        public ActionResult Create() {
+            
+            return View();
+        
+        }
+        public IActionResult Index()
+        {
+            var course = _courseService.GetAllCourses();
+            return View(course);
+        }
+    }
+}
