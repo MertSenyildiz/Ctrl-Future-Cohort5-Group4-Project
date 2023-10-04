e9ad46c287364ec1268fbc6014dd1a7b2797bb35
Metehan İNAL <metehan.inal25@gmail.com>
2023-10-04 11:33:05 +0300

    Basic InstructorController added

---
+++ Project/Controllers/InstructorController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Project.Controllers
+{
+    public class InstructorController : Controller
+    {
+
+        [Authorize(Roles = "Student")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
