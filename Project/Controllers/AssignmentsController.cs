4bcc91ec44e11b05d8b455567e85b90dccc32739
MertSenyildiz <mert.senyildiz123@gmail.com>
2023-10-08 14:45:06 +0300

    some fixes

---
+++ Project/Controllers/AssignmentsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Project.Business.Abstract;
+using Project.Models;
+
+namespace Project.Controllers
+{
+    public class AssignmentsController : Controller
+    {
+        IAssignmentService _assignmentService;
+        public AssignmentsController(IAssignmentService assignmentService)
+        {
+            _assignmentService = assignmentService;
+        }
+        [Authorize(Roles ="Admin,Instructor")]
+        [HttpPost]
+        public ActionResult CreateAssignment(Assignment assignment)
+        {
+            _assignmentService.CreateAssignment(assignment);
+            return RedirectToAction($"{assignment.CourseID}","Course");
+        }
+
+        [Authorize(Roles = "Admin,Instructor")]
+        [HttpPost]
+        public ActionResult DeleteAssignment(string assignmentId,string courseId)
+        {
+            _assignmentService.DeleteAssignment(Guid.Parse(assignmentId));
+            return RedirectToAction($"{courseId}", "Course");
+        }
+    }
+}
